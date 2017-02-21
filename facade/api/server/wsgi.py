0194e816424fb913ec1c92f43c0fc40b9702b52c
Schlueter <bs@schlueter.blue>
2017-02-20 19:37:53 -0500

    Add server/list endpoint

--- facade/api/server/wsgi.py
+++ facade/api/server/wsgi.py
@@ -1,13 +1,38 @@
 # Copyright 2017 Brandon Schlueter
 # pylint: disable=no-self-use
 
+import os
+
 from flask import Flask
 from flask_restful import reqparse, Api, Resource
 
+from novaclient import client
+
+
+OS_COMPUTE_VERSION = os.environ.get('OS_COMPUTE_VERSION', 2)
+OS_USERNAME = os.environ.get('OS_USERNAME')
+OS_PASSWORD = os.environ.get('OS_PASSWORD')
+OS_PROJECT_NAME = os.environ.get('OS_PROJECT_NAME')
+OS_AUTH_URL = os.environ.get('OS_AUTH_URL')
+OS_PROJECT_DOMAIN_NAME = os.environ.get('OS_PROJECT_DOMAIN_NAME', 'default')
+OS_USER_DOMAIN_NAME = os.environ.get('OS_USER_DOMAIN_NAME', 'default')
+
+NOVA = client.Client(version=OS_COMPUTE_VERSION,
+                     username=OS_USERNAME,
+                     password=OS_PASSWORD,
+                     project_name=OS_PROJECT_NAME,
+                     auth_url=OS_AUTH_URL,
+                     project_domain_name=OS_PROJECT_DOMAIN_NAME,
+                     user_domain_name=OS_USER_DOMAIN_NAME)
+
 class Ping(Resource):
     def get(self):
-        return "pong"
+        return {"response": "pong"}
 
+class ServerList(Resource):
+
+    def get(self):
+        return [server.to_dict() for server in NOVA.servers.list()]
 
 def initialize_application():
 
@@ -18,5 +43,6 @@ def initialize_application():
     parser.add_argument('task')
 
     api.add_resource(Ping, '/ping')
+    api.add_resource(ServerList, '/server/list')
 
     app.run(debug=True)
