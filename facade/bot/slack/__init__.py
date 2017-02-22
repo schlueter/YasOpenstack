dfb4ece747c4aa5e2058f0a615689595010983ea
Schlueter <bs@schlueter.blue>
2017-02-22 16:33:37 -0500

    Add initial slackbot

---
+++ facade/bot/slack/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+import os
+
+from slackclient import SlackClient
+
+SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+BOT_NAME = 'facade'
+
+def init():
+    slack_client = SlackClient(SLACK_BOT_TOKEN)
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
+
+if __name__ == '__main__':
+    init()
