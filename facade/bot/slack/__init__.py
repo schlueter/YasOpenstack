f94f5eebecb57585b8a0cea686f9a746f4b6d2b2
Schlueter <bs@schlueter.blue>
2017-02-23 17:51:47 -0500

    Add mock handlers and some other stuff

--- facade/bot/slack/__init__.py
+++ facade/bot/slack/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import os
+import re
 import sys
 import time
 
@@ -13,11 +14,45 @@ AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
 BOT_NAME = 'facade'
 
-class Client(object):
+def list_handler(target):
+    return 'listing'
+
+def launch_handler(name, branch):
+    return f'launching {name} on {branch}'
+
+def delete_handler(name):
+    return f'deleting {name}'
+
+HANDLERS = {
+    re.compile('(?:list)\ ?(\w\.)'): list_handler,
+    re.compile('(?:launch|start)\ ([-\w]+)(?:\ on\ )?([-\w]+:?[-\w]+)?'): launch_handler,
+    re.compile('(?:delete|drop|terminate|bust a cap in|pop a cap in) ([-\ \w]+)'): delete_handler
+}
+
+def log(*msgs):
+    print(*msgs, file=sys.stderr)
+
+class Client(SlackClient):
 
     def __init__(self):
-        self.slack_client = SlackClient(BOT_TOKEN)
+        super().__init__(BOT_TOKEN)
+        self.bot_id = self.__retrieve_bot_user_id()
+        self.at_bot = "<@" + self.bot_id + ">"
 
+    def __retrieve_bot_user_id(self):
+        log("Retrieving users list.")
+        api_call = self.api_call("users.list")
+        if api_call.get('ok'):
+            # retrieve all users so we can find our bot
+            users = api_call.get('members')
+            log(f"Pulling the bot's, {BOT_NAME}, ID.")
+            for user in users:
+                if 'name' in user and user.get('name') == BOT_NAME:
+                    return user.get('id')
+            else:
+                raise NoBot("could not find bot user with the name " + BOT_NAME)
+        else:
+            raise SlackClientFailure()
 
     def handle_command(self, command, channel):
         """
@@ -27,9 +62,17 @@ class Client(object):
         """
         response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
                    "* command with numbers, delimited by spaces."
-        if command.startswith(EXAMPLE_COMMAND):
+
+        for regex in HANDLERS.keys():
+            match = regex.match(command)
+            if match:
+                groups = match.groups()
+                return self.reply(channel, HANDLERS[regex](*groups))
+        else:
             response = "Sure...write some more code then I can do that!"
-        self.slack_client.api_call(
+
+    def reply(self, channel, response):
+        return self.api_call(
             "chat.postMessage",
             channel=channel,
             text=response,
@@ -48,27 +91,27 @@ class Client(object):
                                   and 'channel' in output
                                   and 'text' in output
                                   and not output.get('user') == BOT_ID]:
-                    print(f"rtm_output {rtm_output}")
-                    channel_info = self.slack_client.api_call('channels.info', channel=output.get('channel'))
-                    if channel_info.get('ok', False) and AT_BOT in output['text']:
-                        print(f"receieved message in {output['channel']} from {output['user']}: {output['text']}", file=sys.stderr)
-                        return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
-                    group_info = self.slack_client.api_call('groups.info', channel=output.get('channel'))
-                    if not channel_info.get('ok', False) and not group_info.get('ok', False):
-                        print(f"receieved direct message from {output['user']}: {output['text']}", file=sys.stderr)
-                        return output['text'].strip().lower(), output['channel']
+                log(f"rtm_output {rtm_output}")
+                channel_info = self.api_call('channels.info', channel=output.get('channel'))
+                if channel_info.get('ok', False) and AT_BOT in output['text']:
+                    log(f"receieved message in {output['channel']} from {output['user']}: {output['text']}")
+                    return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
+                group_info = self.api_call('groups.info', channel=output.get('channel'))
+                if not channel_info.get('ok', False) and not group_info.get('ok', False):
+                    log(f"receieved direct message from {output['user']}: {output['text']}")
+                    return output['text'].strip().lower(), output['channel']
         return None, None
 
     def listen(self):
-        if self.slack_client.rtm_connect():
-            print("StarterBot connected as {}<{}> and running!".format(BOT_NAME, BOT_ID))
+        if self.rtm_connect():
+            log("StarterBot connected as {}<{}> and running!".format(BOT_NAME, BOT_ID))
             while True:
-                command, channel = self.parse_slack_output(self.slack_client.rtm_read())
+                command, channel = self.parse_slack_output(self.rtm_read())
                 if command and channel:
                     self.handle_command(command, channel)
                 time.sleep(READ_WEBSOCKET_DELAY)
         else:
-            print("Connection failed. Invalid Slack token or bot ID?")
+            log("Connection failed. Invalid Slack token or bot ID?")
 
 def main():
     client = Client()
